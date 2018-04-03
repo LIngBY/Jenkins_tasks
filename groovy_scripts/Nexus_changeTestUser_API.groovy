8e48ac989b091fb2b4d6cbd15988082a8a30d653
Eugene Hodin <mushtarda@gmail.com>
2018-04-03 08:47:33 +0300

    Create Nexus_changeTestUser_API.groovy

---
+++ groovy_scripts/Nexus_changeTestUser_API.groovy
@@ -0,0 +1,6 @@
+{
+  "name": "testUser",
+  "content": "def user = security.securitySystem.getUser('test');user.setEmailAddress('test@mycompany.com');security.securitySystem.updateUser(user);security.securitySystem.changePassword('test','admin');",
+  "type": "groovy"
+}
+
